db4910f7343b91e5b02eb4e18570019a179a8f6c
Noodle <115919945+noodle02846@users.noreply.github.com>
2026-08-21 12:31:27 -0500

    fix(discord): allow unfree predicate

--- modules/home-manager/programs/discord.nix
+++ modules/home-manager/programs/discord.nix
@@ -1,5 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   home.packages = with pkgs; [
     discord
   ];
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "discord"
+    ];
 }
